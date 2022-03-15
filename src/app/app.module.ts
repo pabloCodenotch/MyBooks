afc21c159416a8d71da805e941c0b07e0da470ed
pabloCodenotch <seriusmail13@gmail.com>
2022-03-15 19:01:00 +0100

    Saving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './component/head/head.component';
+import { FootComponent } from './component/foot/foot.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    FootComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
